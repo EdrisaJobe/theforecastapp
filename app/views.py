3fe4595a2d9942c5bf915840c5a626c747590d58
Edrisa Jobe <ed.jobe@yahoo.com>
2023-01-04 23:35:00 -0500

    UI update

--- app/views.py
+++ app/views.py
@@ -6,16 +6,16 @@ import requests
 
 def index(request):
 
-    # if request.method == "POST":
+    if request.method == "POST":
         
-    #     try:
-    #         # post based on the city
-    #         city = request.POST['city']
-    #         api_url = requests.get('https://api.openweathermap.org/data/2.5/weather?q=' +
-    #                             city + '&units=metric&appid=aa237aa0b8bfefa9b4156b04460b1e24')
+        try:
+            # post based on the city
+            city = request.POST['city']
+            api_url = requests.get('https://api.openweathermap.org/data/2.5/weather?q=' +
+                                city + '&units=imperial&appid=aa237aa0b8bfefa9b4156b04460b1e24')
 
-    #         data_list = api_url.json()
-    #     except:
-    #         return redirect('/')
+            data_list = api_url.json()
+        except:
+            return redirect('/')
 
-    return render(request, 'app/index.html')
+    return render(request, 'app/index.html', {'data_list':data_list})
